bb80b35e57df812aed9adff89fdc3d610f432eb9
Laurel Laoang, Jr <laurellaoang@gmail.com>
2024-06-09 19:24:27 +0200

    Initial Commit

---
+++ view/viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  viewApp.swift
+//  view
+//
+//  Created by Laurel Laoang, Jr on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
